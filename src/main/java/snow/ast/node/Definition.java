3d961d79c800a1e377fc3fb0bfe0fab940ddcf69
Cy Brown <cy.brown59@gmail.com>
2015-05-03 22:10:43 +0200

    Added optional type on definition

--- src/main/java/snow/ast/node/Definition.java
+++ src/main/java/snow/ast/node/Definition.java
@@ -5,23 +5,28 @@ import lombok.Value;
 import snow.IAstVisitor;
 import snow.ast.Token;
 
+import java.util.Optional;
+
 @Value
 @EqualsAndHashCode(callSuper = true)
 public class Definition extends BaseAstNode {
 
     private final Identifier identifier;
+    private final Optional<BaseAstNode> type;
     private final BaseAstNode value;
 
-    public Definition(Token firstToken, Token lastToken, Identifier identifier, BaseAstNode value) {
+    public Definition(Token firstToken, Token lastToken, Identifier identifier, BaseAstNode value, BaseAstNode type) {
         super(firstToken, lastToken);
         this.identifier = identifier;
         this.value = value;
+        this.type = Optional.ofNullable(type);
     }
 
     @Override
     public void accept(IAstVisitor visitor) {
         visitor.visit(this, true);
         this.identifier.accept(visitor);
+        this.type.ifPresent(t -> t.accept(visitor));
         this.value.accept(visitor);
         visitor.visit(this, false);
     }

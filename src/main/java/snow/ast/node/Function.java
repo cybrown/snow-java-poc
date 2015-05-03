0f6c80ed87427c74bbc327a15394c16c3028689d
Cy Brown <cy.brown59@gmail.com>
2015-05-03 22:36:08 +0200

    Added function and call argument

--- src/main/java/snow/ast/node/Function.java
+++ src/main/java/snow/ast/node/Function.java
@@ -5,22 +5,30 @@ import lombok.Value;
 import snow.IAstVisitor;
 import snow.ast.Token;
 
+import java.util.Optional;
+
 @Value
 @EqualsAndHashCode(callSuper = true)
 public class Function extends BaseAstNode {
 
     private BaseAstNode body;
     private String name;
+    private Optional<Identifier> argName;
+    private Optional<BaseAstNode> argType;
 
-    public Function(Token firstToken, Token lastToken, String name, BaseAstNode body) {
+    public Function(Token firstToken, Token lastToken, String name, BaseAstNode body, Identifier argName, BaseAstNode argType) {
         super(firstToken, lastToken);
         this.name = name;
         this.body = body;
+        this.argName = Optional.ofNullable(argName);
+        this.argType = Optional.ofNullable(argType);
     }
 
     @Override
     public void accept(IAstVisitor visitor) {
         visitor.visit(this, true);
+        this.argName.ifPresent(n -> n.accept(visitor));
+        this.argType.ifPresent((t -> t.accept(visitor)));
         this.body.accept(visitor);
         visitor.visit(this, false);
     }

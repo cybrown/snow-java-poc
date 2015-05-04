f53ee9391961cfc710bac808eb7b137d271703a0
Cy Brown <cy.brown59@gmail.com>
2015-05-04 18:58:16 +0200

    Classes with body

--- src/main/java/snow/ast/node/SnowClass.java
+++ src/main/java/snow/ast/node/SnowClass.java
@@ -1,23 +1,30 @@
 package snow.ast.node;
 
+import lombok.EqualsAndHashCode;
+import lombok.Value;
 import snow.IAstVisitor;
 import snow.ast.Token;
 
 import java.util.Optional;
 
+@Value
+@EqualsAndHashCode(callSuper = true)
 public class SnowClass extends BaseAstNode {
 
     private final Identifier identifier;
+    private final Optional<BaseAstNode> body;
 
-    public SnowClass(Token firstToken, Token lastToken, Identifier identifier) {
+    public SnowClass(Token firstToken, Token lastToken, Identifier identifier, BaseAstNode body) {
         super(firstToken, lastToken);
         this.identifier = identifier;
+        this.body = Optional.ofNullable(body);
     }
 
     @Override
     public void accept(IAstVisitor visitor) {
         visitor.visit(this, true);
         this.identifier.accept(visitor);
+        this.body.ifPresent(b -> b.accept(visitor));
         visitor.visit(this, false);
     }
 }
